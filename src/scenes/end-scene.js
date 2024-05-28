b777987211a6648b1cb08f52d210c24d84dceac2
Alexandra <alexandra.oreza@gmail.com>
2024-05-28 16:50:22 -0600

    fixed issues with progress scene

--- src/scenes/end-scene.js
+++ src/scenes/end-scene.js
@@ -1,58 +1,89 @@
 import Phaser from "../lib/phaser.js";
+import { ProgressScene } from "./progress-scene.js";
 import { SCENE_KEYS } from "./scene-keys.js";
 
-export class EndScene extends Phaser.Scene{
-    constructor() {
-        super({
-          key: SCENE_KEYS.END_SCENE,
-        });
-      }
-      
-    create(){
-      console.log(`[${EndScene.name}: create] invoked`);
-      console.log('Yei, no hay más niveles');
+export class EndScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: SCENE_KEYS.END_SCENE,
+    });
+  }
 
-      this.add.text(200, 200, "Felicidades, has terminado \ntodos los niveles",{
-        fontSize: '40px',
-        color: 'white',
-        align: 'center',
-      }).setOrigin(0); 
+  create() {
+    console.log(`[${EndScene.name}: create] invoked`);
+    console.log("Yei, no hay más niveles");
 
-      this.add.text(5, 535, "Assets empleados:\nComplete UI Essential Pack (crusenho)\nHand drawn dungeon icon pack (limofeus)",{
-        fontSize: '15px',
-        color: 'white',
-      }).setOrigin(0); 
+    this.add
+      .text(200, 200, "Felicidades, has terminado \ntodos los niveles", {
+        fontSize: "40px",
+        color: "white",
+        align: "center",
+      })
+      .setOrigin(0);
 
-      this.add.text(785, 535, "sfx empleados:\nCDat's Wrong! (Beetlemuse)\nDat's Right! (Beetlemuse)",{
-        fontSize: '15px',
-        color: 'white',
-        align: 'right'
-      }).setOrigin(0); 
+    this.add
+      .text(
+        5,
+        535,
+        "Assets empleados:\nComplete UI Essential Pack (crusenho)\nHand drawn dungeon icon pack (limofeus)",
+        {
+          fontSize: "15px",
+          color: "white",
+        }
+      )
+      .setOrigin(0);
 
+    this.add
+      .text(
+        785,
+        535,
+        "sfx empleados:\nCDat's Wrong! (Beetlemuse)\nDat's Right! (Beetlemuse)",
+        {
+          fontSize: "15px",
+          color: "white",
+          align: "right",
+        }
+      )
+      .setOrigin(0);
 
-      const button = this.add.text(370, 300, 'Volver al menu', {
-        fontFamily: 'Courier',
-        fontSize: '15px',
-        color: '#ffffff',
-        align: 'center',
+    const button = this.add
+      .text(370, 300, "Volver al menu", {
+        fontFamily: "Courier",
+        fontSize: "15px",
+        color: "#ffffff",
+        align: "center",
         fixedWidth: 260,
-        backgroundColor: '#8e8e8e'
-    }).setPadding(32).setOrigin(0);
+        backgroundColor: "#8e8e8e",
+      })
+      .setPadding(32)
+      .setOrigin(0);
 
     button.setInteractive({ useHandCursor: true });
 
-      //button events
-      button.on('pointerover', () => {
-          button.setBackgroundColor('#c1c1c1');
-      });
+    //button events
+    button.on("pointerover", () => {
+      button.setBackgroundColor("#c1c1c1");
+    });
 
-      button.on('pointerout', () => {
-          button.setBackgroundColor('#8e8e8e');
-      });
-      button.on('pointerup', () => {
-          this.sound.stopAll();
-          this.scene.start(SCENE_KEYS.MENU_SCENE);
-          button.disableInteractive();
-      }, this);
-    }
+    button.on("pointerout", () => {
+      button.setBackgroundColor("#8e8e8e");
+    });
+    button.on(
+      "pointerup",
+      () => {
+        this.sound.stopAll();
+        const progressScene = this.scene.get(SCENE_KEYS.PROGRESS_SCENE);
+        if (progressScene && progressScene instanceof ProgressScene) {
+          progressScene.resetProgress();
+        } else {
+          console.error(
+            "ProgressScene is not initialized or is not an instance of ProgressScene"
+          );
+        }
+        this.scene.start(SCENE_KEYS.MENU_SCENE);
+        button.disableInteractive();
+      },
+      this
+    );
+  }
 }
--- src/scenes/gameover-scene.js
+++ src/scenes/gameover-scene.js
@@ -2,40 +2,57 @@ import Phaser from "../lib/phaser.js";
 import { ProgressScene } from "./progress-scene.js";
 import { SCENE_KEYS } from "./scene-keys.js";
 
-export class GameOverScene extends Phaser.Scene{
-    constructor(){
-        super({
-            key: SCENE_KEYS.GAMEOVER_SCENE,
-        })
-    }
-    create(){
-        this.add.text(250, 150, "Vaya, parece que no has comprendido el tema lo suficiente :(");
+export class GameOverScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: SCENE_KEYS.GAMEOVER_SCENE,
+    });
+  }
+  create() {
+    this.add.text(
+      250,
+      150,
+      "Vaya, parece que no has comprendido el tema lo suficiente :("
+    );
 
-        const button = this.add.text(370, 300, 'Volver al menu', {
-            fontFamily: 'Courier',
-            fontSize: '15px',
-            color: '#ffffff',
-            align: 'center',
-            fixedWidth: 260,
-            backgroundColor: '#8e8e8e'
-        }).setPadding(32).setOrigin(0);
-  
-        button.setInteractive({ useHandCursor: true });
-  
-          //button events
-          button.on('pointerover', () => {
-              button.setBackgroundColor('#c1c1c1');
-          });
-  
-          button.on('pointerout', () => {
-              button.setBackgroundColor('#8e8e8e');
-          });
-          button.on('pointerup', () => {
-              this.sound.stopAll();
-              let progress = new ProgressScene();
-              progress.resetProgress();
-              this.scene.start(SCENE_KEYS.MENU_SCENE);
-              button.disableInteractive();
-          }, this);
-    }
+    const button = this.add
+      .text(370, 300, "Volver al menu", {
+        fontFamily: "Courier",
+        fontSize: "15px",
+        color: "#ffffff",
+        align: "center",
+        fixedWidth: 260,
+        backgroundColor: "#8e8e8e",
+      })
+      .setPadding(32)
+      .setOrigin(0);
+
+    button.setInteractive({ useHandCursor: true });
+
+    //button events
+    button.on("pointerover", () => {
+      button.setBackgroundColor("#c1c1c1");
+    });
+
+    button.on("pointerout", () => {
+      button.setBackgroundColor("#8e8e8e");
+    });
+    button.on(
+      "pointerup",
+      () => {
+        this.sound.stopAll();
+        const progressScene = this.scene.get(SCENE_KEYS.PROGRESS_SCENE);
+        if (progressScene && progressScene instanceof ProgressScene) {
+          progressScene.resetProgress();
+        } else {
+          console.error(
+            "ProgressScene is not initialized or is not an instance of ProgressScene"
+          );
+        }
+        this.scene.start(SCENE_KEYS.MENU_SCENE);
+        button.disableInteractive();
+      },
+      this
+    );
+  }
 }
